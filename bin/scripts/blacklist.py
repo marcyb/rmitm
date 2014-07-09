68b16e7eb5f609023e359213e44b864fb8008e36
Marc Bleeze <marc.bleeze@itv.com>
2014-07-09 10:55:48 +0100

    change blacklist.py to respond directly without relaying request to server

--- bin/scripts/blacklist.py
+++ bin/scripts/blacklist.py
@@ -1,10 +1,22 @@
 import sys, getopt, re
+from netlib.odict import ODictCaseless
+from libmproxy.flow import Response
 
-def response(context, flow):
+# def response(context, flow):
+# 	if path.search(flow.request.path) is not None:
+# 		flow.response.code = 404
+# 		flow.response.msg = "Not Found"
+#		flow.response.content = "Blacklisted"
+
+def request(context, flow):
 	if path.search(flow.request.path) is not None:
-		flow.response.code = 404
-		flow.response.msg = "Not Found"
-		flow.response.content = "Oooops!!"
+			resp = Response(
+				flow.request,
+				[1,1], 404, "Not Found",
+				ODictCaseless([["Content-Type","text/html"],["Server","Apache/2.4.9 (Unix)"]]),
+				"Blacklisted",
+				None)
+			flow.request.reply(resp)
 
 def start(context, argv):
 	try:
