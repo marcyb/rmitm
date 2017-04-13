a592bb4614350cf678a36dcca65e407e00283549
Dan Oades <daniel.oades@itv.com>
2017-04-13 11:03:17 +0100

    temp revert port change

--- lib/mitmdump.rb
+++ lib/mitmdump.rb
@@ -112,35 +112,10 @@ class Mitmdump
 
 	private
 
-=begin
 	def port_available?
 		# `nc -z 127.0.0.1 #{@port} >& /dev/null`
 		system("nc -z 127.0.0.1 #{@port}#{' &> /dev/null' unless RbConfig::CONFIG['host_os'] == 'linux'}")
 		!$?.success?
-	end
-=end
-
-	def port_available?
-		ip = '127.0.0.1'
-		port = @port
-		s = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
-		sa = Socket.sockaddr_in(port, ip)
-
-		begin
-			s.connect_nonblock(sa)
-		rescue Errno::EINPROGRESS
-			if IO.select(nil, [s], nil, 1)
-				begin
-					s.connect_nonblock(sa)
-				rescue Errno::EISCONN
-					return true
-				rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-					return false
-				end
-			end
-		end
-
-		return false
 	end
 
 	def command
